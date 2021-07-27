549561809b590f31562a832065786029c4baa9be
Łukasz Zalewski <lukaszzalewski1820@wp.pl>
2021-07-27 23:15:11 +0200

    Add project files.

---
+++ DCP/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace DCP
+{
+    class Program
+    {
+        static int[] array = new int[] { 10, 5, 2, 7, 8, 7 };
+        static int k = 3;
+        static void Main(string[] args)
+        {
+            FindMax(array, k, array.Length);
+        }
+
+        public static void FindMax(int[] array, int k, int n)
+        {
+            int max;
+            for (int i = 0; i <= n - k; i++)
+            {
+                max = array[i];
+
+                for (int j = 1; j < k; j++)
+                {
+                    if (array[i + j] > max)
+                        max = array[i + j];
+                }
+                Console.Write(max + " ");
+            }
+
+        }
+    }
+}
